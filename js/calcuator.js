d6766a8c8fa7d72e56669f5ba32fe3ccb03108d0
deeppaz <deeppaz@hotmail.com>
2023-07-30 21:20:25 +0300

    comment in description

--- js/calcuator.js
+++ js/calcuator.js
@@ -26,13 +26,49 @@ function hesapla() {
   var ogrOrtalama = (ogrVizeNot * 0.3) + (ogrFinalNot * 0.7);
   ogrOrtalama = ogrOrtalama.toFixed(2);
 
+  if (ogrOrtalama > '0') {
+    document.getElementsByClassName("sonuc")[0].style.display = "block";
+    document.getElementsByClassName("temizle")[0].style.display = "block"
+  }
+
   if (ogrOrtalama >= 0 && ogrOrtalama <= 30) {
     document.getElementById("sonuc").textContent = "Not ortalamanız: " + ogrOrtalama + " DD - KALDINIZ";
+    document.body.style.background = "red";
   } else if (ogrOrtalama >= 31 && ogrOrtalama <= 49) {
     document.getElementById("sonuc").textContent = "Not ortalamanız: " + ogrOrtalama + " DC - KOŞULLU";
+    document.body.style.background = "yellow";
   } else if (ogrOrtalama >= 50 && ogrOrtalama <= 84) {
     document.getElementById("sonuc").textContent = "Not ortalamanız: " + ogrOrtalama + " CC - GEÇTİNİZ";
+    document.body.style.background = "green";
   } else if (ogrOrtalama >= 85 && ogrOrtalama <= 100) {
     document.getElementById("sonuc").textContent = "Not ortalamanız: " + ogrOrtalama + " AA - GEÇTİNİZ";
+    document.body.style.background = "green";
   }
+}
+
+const temizle = () => {
+  var ogrVizeNot = parseInt(document.getElementById("vize").value);
+  var ogrFinalNot = parseInt(document.getElementById("final").value);
+  var ogrOrtalama = (ogrVizeNot * 0.3) + (ogrFinalNot * 0.7);
+  ogrOrtalama = ogrOrtalama.toFixed(2);
+  if (ogrOrtalama >= 0 && ogrOrtalama <= 100) {
+    parseInt(document.getElementById("vize").value = '');
+    parseInt(document.getElementById("final").value = '');
+   
+    document.body.style.background = "linear-gradient(112deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)"
+    document.getElementById("sonuc").textContent = ""
+    document.getElementsByClassName("sonuc")[0].style.display = "none";
+    document.getElementsByClassName("temizle")[0].style.display = "none"
+  }
+}
+
+var ogrVizeNot = document.getElementById("vize").value;
+var ogrFinalNot = document.getElementById("final").value;
+var ogrOrtalama = (ogrVizeNot * 0.3) + (ogrFinalNot * 0.7);
+ogrOrtalama = ogrOrtalama.toFixed(0);
+
+
+if (ogrOrtalama === '0') {
+  document.getElementsByClassName("sonuc")[0].style.display = "none";
+  document.getElementsByClassName("temizle")[0].style.display = "none"
 }
